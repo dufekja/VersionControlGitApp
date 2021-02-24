7a0d3ccf58067ea35e34e523a86f269253391b0e
dufekja <jan.dufek2002@gmail.com>
2021-02-24 14:21:50 +0100

    token window comments

--- VersionControlGitApp/Windows/TokenWindow.xaml.cs
+++ VersionControlGitApp/Windows/TokenWindow.xaml.cs
@@ -21,11 +21,17 @@ namespace VersionControlGitApp {
         private PrivateTokenDB tokenDB;
         public static string user;
 
+        /// <summary>
+        /// Token window construcotr
+        /// </summary>
         public TokenWindow() {
+
+            // get logged user and init token database
             user = System.Windows.Forms.SystemInformation.UserName;
             tokenDB = new PrivateTokenDB();
             tokenDB.InitDB();
 
+            // if user have active token then go to main window
             List<Token> tokens = tokenDB.FindTokensByUser(user);
             if (tokens != null) {
                 foreach (Token token in tokens) {
@@ -38,15 +44,27 @@ namespace VersionControlGitApp {
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Go to main window action
+        /// </summary>
+        /// <param name="token">Token value</param>
         private void GoMainWindow(string token) {
             new MainWindow(token, tokenDB).Show();
-            this.Close();
+            Close();
         }
 
+        /// <summary>
+        /// Submit new token action
+        /// </summary>
+        /// <param name="sender">Object that triggered action</param>
+        /// <param name="e">All added arguments</param>
         private void SubmitToken(object sender, RoutedEventArgs e) {
             string token = TokenTextBox.Text.ToLower();
 
+            // check token lenght
             if (token.Length == 40) {
+
+                // if token is not in database
                 if (tokenDB.FindTokenByValue(token) == null) {
                     Token activeToken = tokenDB.GetActiveToken(user);
                     int isActive = 0;
@@ -64,22 +82,42 @@ namespace VersionControlGitApp {
             }
         }
 
+        /// <summary>
+        /// Request navigation to website
+        /// </summary>
+        /// <param name="sender">Object that triggered action</param>
+        /// <param name="e">All added arguments</param>
         private void Hyperlink_RequestNavigate(object sender, System.Windows.Navigation.RequestNavigateEventArgs e) {
             Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
             e.Handled = true;
         }
 
+        /// <summary>
+        /// Minimize window action
+        /// </summary>
+        /// <param name="sender">Object that triggered action</param>
+        /// <param name="e">All added arguments</param>
         private void Window_Minimized(object sender, RoutedEventArgs e) {
             WindowState = WindowState.Minimized;
         }
 
+        /// <summary>
+        /// Close window action
+        /// </summary>
+        /// <param name="sender">Object that triggered action</param>
+        /// <param name="e">All added arguments</param>
         private void Window_Closed(object sender, RoutedEventArgs e) {
-            this.Close();
+            Close();
         }
 
+        /// <summary>
+        /// Drag window on mouse down action
+        /// </summary>
+        /// <param name="sender">Object that triggered action</param>
+        /// <param name="e">All added arguments</param>
         private void DragWindownOnMouseDown(object sender, MouseButtonEventArgs e) {
             if (Mouse.LeftButton == MouseButtonState.Pressed)
-                this.DragMove();
+                DragMove();
         }
 
     }
